69170137a66d0c6e3367982221ef31ef27951005
MadJak91 <madjak91@gmail.com>
2017-03-14 19:33:19 +0100

    Test OpenCV

--- main.cpp
+++ main.cpp
@@ -2,11 +2,16 @@
 using namespace std;
 
 #include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 using namespace cv;
 
 int main()
 {
+	Mat obrazok = imread("test.png", 1);
 	
+    namedWindow("Okno", CV_WINDOW_AUTOSIZE);
+    imshow("Obrazok", obrazok);
+    waitKey(0);
 
 	return 0;
 }

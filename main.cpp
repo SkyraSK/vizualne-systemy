29b7d307fcdf3a5c6801963b7214e80662df6145
MadJak91 <madjak91@gmail.com>
2017-03-21 18:05:54 +0100

    ukladanie snimok

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <sstream>
 using namespace std;
 
 #include <opencv2/core/core.hpp>
@@ -16,16 +18,30 @@ int main()
 		cout << "Prava kamera nefunguje.\n";
 
     Mat obrazok_lavy, obrazok_pravy;
-    int vstup;
-    while (true)
+    int vstup = 'a', poradie_snimky = 1;
+    while (vstup != 27) //ESC koniec
     {
         lava_kamera >> obrazok_lavy;
         prava_kamera >> obrazok_pravy;
         imshow("Lava snimka", obrazok_lavy);
         imshow("Prava snimka", obrazok_pravy);
         vstup = waitKey(1);
-        if (vstup == 27)
+        
+        switch (vstup)
+        {
+        default:
 			break;
+		case 27: //ESC
+			break;
+		case 'f':
+			ostringstream oss;
+			oss << poradie_snimky;
+			imwrite("snimky/snimkaLava" + oss.str() + ".jpg", obrazok_lavy);
+			imwrite("snimky/snimkaPrava" + oss.str() + ".jpg", obrazok_pravy);
+			poradie_snimky++;
+			break;
+        }
+		
     }
 	lava_kamera.release();
 	prava_kamera.release();
